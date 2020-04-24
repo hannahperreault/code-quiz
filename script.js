fc5c3b2eedd29781b07a115efe4097ed9c90f2f9
Hannah Perreault <hperre@gmail.com>
2020-04-24 14:58:36 -0400

    wrote out pseudocode

---
+++ script.js
@@ -0,0 +1,36 @@
+//*when first starting the page, they see an intro message
+//in the html, create div with intro
+
+//*variable to store the current count, set at zero for default
+//variable to store the array of the questions
+//variable to store the index of the current question they are answering
+
+
+//*click button to start
+//when user clicks start, hide the intro div
+//create setInterval of 75000ms 
+//update teh count to 75
+//every time the interval runs, we need to decrement the current count by 1, re-render the current count
+//
+
+//render the first question
+    //the question (string)
+    //array of possible answers
+    //correct answer (number, index of the answer of the possible answers)
+    //using DOM manip, create a new el with the questino info and display it on the screen appendChild.
+
+
+
+//once game starts, display with buttons for the answers
+///when clicked, determine if correct/in
+//use data attributes to tell if button clicked is right (with event listener)
+//display next wquestions, and display status of their answer for a short period of time
+
+
+//end condition--
+    //create function for end game by either condition. call the function to display end page if:
+//user answers all questions OR
+//time runs out
+
+//when game ends, allow user to enter their initials and store their score
+//redirect user to high scores page, which dsiplays all high scores
